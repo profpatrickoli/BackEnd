1fc241d54ab6e996e7f7a7287b8c37f27169ba12
profpatrickoli <patrick.staroin@escola.pr.gov.br>
2024-03-04 08:59:24 -0300

    Atividade no caderno

---
+++ atividade1.js
@@ -0,0 +1,35 @@
+// 1)
+console.log(true == 1);
+
+// 2)
+console.log(false == 0);
+
+// 3)
+console.log(false == '');
+
+// 4)
+console.log(true === 1);
+
+// 5)
+console.log(false === 0);
+
+// 6)
+console.log(10 + "10");
+
+// 7)
+console.log(10 * "10");
+
+// 8)
+console.log(10 == "10");
+
+// 9)
+console.log(10 === "10");
+
+// 10)
+console.log(10 === Number("10"));
+
+// 11)
+console.log("Teste" * 10);
+
+
+
